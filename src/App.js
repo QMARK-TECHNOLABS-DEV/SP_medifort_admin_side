6148ce00ba27ab2e8b2fb91d4ae6afed730039eb
devi-priya22 <sdevipriya803@gmail.com>
2024-08-16 11:09:08 +0530

    Banner complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DoctorProfiles from './pages/doctorProfile/DoctorProfiles';
 import DoctorDetailedView from './pages/doctorProfile/DoctorDetailedView';
+import BannerCo from './pages/bannerComp/BannerCo';
+import BannComp from './pages/bann.Comp/BannComp';
 import MainLayout from './components/layout/MainLayout';
 
 function App() {
@@ -18,6 +20,13 @@ function App() {
             {/* Doctor Profile part */}
             <Route path='/doctor-profiles' element={<DoctorProfiles />} />
             <Route path='/doctor-profiles/:id' element={<DoctorDetailedView />} />
+
+            {/* Banner */}
+            <Route path='/banner' element={<BannerCo />} />
+            <Route path='/add-banner' element={<BannComp />} />
+
+
+
 
 
           </Routes>
