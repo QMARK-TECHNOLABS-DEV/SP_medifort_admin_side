282666033e9648c301c9c1dbe569b56b97f7b8d6
Manu M <manum66466@gmail.com>
2024-08-20 10:17:16 +0530

    Update the Breadcrumbs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
       colors: {
         primaryColor: "#9C2677",
         lightGray: "#E8E8E8"
-      }
+      },
+      fontSize: {
+        'xs': '.75rem',   // 12px
+        'sm': '.875rem',  // 14px
+        'base': '1rem',   // 16px
+        'lg': '1.125rem', // 18px
+        'xl': '1.25rem',  // 20px
+        '2xl': '1.5rem',  // 24px
+        '3xl': '1.875rem',// 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem',    // 48px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.5rem',  // 72px
+        '8xl': '6rem',    // 96px
+        '9xl': '8rem',    // 128px
+      },
     },
   },
   plugins: [],
