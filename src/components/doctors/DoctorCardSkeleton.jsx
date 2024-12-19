3b0df170516a6c1cef83ffe9896eb01b8678bc63
abhijth.r.b <abhijithrb91@gmail.com>
2024-12-19 16:04:18 +0530

    doctor card skeleton size issue fixed

--- src/components/doctors/DoctorCardSkeleton.jsx
+++ src/components/doctors/DoctorCardSkeleton.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const DoctorCardSkeleton = () => (
   <div className="relative w-full h-[350px] bg-gray-200 rounded-3xl border p-8 animate-pulse">
-    {/* Skeleton for image */}
     <div className="w-full h-full bg-gray-300 rounded-2xl" />
-    {/* Skeleton for text content */}
+    
     <div className="absolute inset-x-0 bottom-0 pl-6 pb-2 flex flex-col">
-      <div className="w-1/3 h-4 bg-gray-300 rounded mb-2" /> {/* Department name */}
-      <div className="w-1/2 h-6 bg-gray-300 rounded mb-2" /> {/* Doctor name */}
-      <div className="w-1/4 h-4 bg-gray-300 rounded" />       {/* Location */}
+      <div className="w-1/3 h-4 bg-gray-300 rounded mb-2" /> 
+      <div className="w-1/2 h-6 bg-gray-300 rounded mb-2" /> 
+      <div className="w-1/4 h-4 bg-gray-300 rounded" />       
     </div>
   </div>
 );
