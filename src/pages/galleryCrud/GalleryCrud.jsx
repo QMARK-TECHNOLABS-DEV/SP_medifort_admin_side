3e9455666ee53f898686546f323e401dbaded378
abhijth.r.b <abhijithrb91@gmail.com>
2024-12-23 17:25:14 +0530

    gallery base

--- src/pages/galleryCrud/GalleryCrud.jsx
+++ src/pages/galleryCrud/GalleryCrud.jsx
@@ -1,13 +1,14 @@
 "use client"
-import React, { useState, useRef,useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router";
 import gallery from "../../assets/Gallery/gallery.png";
 import Breadcrumbs from "../../components/common/Breadcrumbs";
 import { toast } from 'react-toastify';
-import { galleryRoute,getAllGalleriesRoute,updateGalleryRoute,uploadRoute } from "../../utils/Endpoint";
+import { galleryRoute, getAllGalleriesRoute, updateGalleryRoute, uploadRoute } from "../../utils/Endpoint";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate"
 import LoadingScreen from "../../components/common/LoadingScreen";
 // import '../../../src/index.css'
+import { RiCloseCircleFill } from "react-icons/ri";
 
 // SVG Trash Icon Component
 const TrashIcon = () => (
@@ -47,7 +48,7 @@ const EditIcon = () => (
 
 
 const Gallery = () => {
-  
+
   const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAddBox, setShowAddBox] = useState(false);
@@ -60,10 +61,11 @@ const Gallery = () => {
   const axiosPrivate = useAxiosPrivate();
   const [galleryIds, setGalleryIds] = useState([]); // Track gallery IDs
   const [editingImageIndex, setEditingImageIndex] = useState(null);
-const [editingImageName, setEditingImageName] = useState("");
-const [editingImageFile, setEditingImageFile] = useState(null);
-const [isModalOpen, setIsModalOpen] = useState(false);
-const [loading, setLoading] = useState(true);
+  const [editingImageName, setEditingImageName] = useState("");
+  const [editingImageFile, setEditingImageFile] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [caption, setCaption] = useState("")
 
   const breadcrumbsItems = [
     { label: "Media", href: "/content-management/media" },
@@ -84,63 +86,63 @@ const [loading, setLoading] = useState(true);
   const toggleAddBox = () => {
     setShowAddBox(prevState => !prevState); // Toggle visibility
   };
-  
-  
+
+
   const handleEditFileChange = (event) => {
     const file = event.target.files[0];
-    
-      setEditingImageFile(file);
+
+    setEditingImageFile(file);
   };
-  
-  
+
+
   const closeModal = () => {
     setIsModalOpen(false);
-};
+  };
 
-//handleUpdateImage
-const handleUpdateImage = async () => {
-  if (editingImageIndex === null) return;
+  //handleUpdateImage
+  const handleUpdateImage = async () => {
+    if (editingImageIndex === null) return;
 
-  const imageId = images[editingImageIndex]._id; // Get the ID from the selected image
-  const formData = new FormData();
-  formData.append('caption', editingImageName); // Add caption to form data
-  if (editingImageFile) {
-    formData.append('image', editingImageFile); // Add image file if provided
-  }
-
-  try {
-    const response = await axiosPrivate.put(`${updateGalleryRoute}/${imageId}`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-
-    // Check the response status and handle it
-    if (response.status !== 200) {
-      toast.error(`Error: ${response.data.msg}`);
-      return;
+    const imageId = images[editingImageIndex]._id; // Get the ID from the selected image
+    const formData = new FormData();
+    formData.append('caption', caption); // Add caption to form data
+    if (editingImageFile) {
+      formData.append('image', editingImageFile); // Add image file if provided
     }
 
-    const updatedImage = response.data.gallery;
+    try {
+      const response = await axiosPrivate.put(`${updateGalleryRoute}/${imageId}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-    // Update the images array with the edited image data
-    const updatedImages = [...images];
-    updatedImages[editingImageIndex] = updatedImage;
-    setImages(updatedImages);
+      // Check the response status and handle it
+      if (response.status !== 200) {
+        toast.error(`Error: ${response.data.msg}`);
+        return;
+      }
 
-    // Close the modal and reset editing state
-    setIsModalOpen(false);
-    setEditingImageIndex(null);
-    setEditingImageName('');
-    setEditingImageFile(null);
-    toast.success(response.data.msg);
-  } catch (error) {
-    console.error("Failed to edit image:", error);
-    toast.error("Something went wrong");
-  }
-};
+      const updatedImage = response.data.gallery;
 
-//handleImageChange
+      // Update the images array with the edited image data
+      const updatedImages = [...images];
+      updatedImages[editingImageIndex] = updatedImage;
+      setImages(updatedImages);
+
+      // Close the modal and reset editing state
+      setIsModalOpen(false);
+      setEditingImageIndex(null);
+      setEditingImageName('');
+      setEditingImageFile(null);
+      toast.success(response.data.msg);
+    } catch (error) {
+      console.error("Failed to edit image:", error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  //handleImageChange
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -153,7 +155,7 @@ const handleUpdateImage = async () => {
       reader.readAsDataURL(file);
     }
   };
-  
+
   //fetGalleryData
   useEffect(() => {
     // Fetch gallery data from the backend
@@ -164,50 +166,50 @@ const handleUpdateImage = async () => {
         if (response?.status === 200) {
           // Assuming `data.galleries` contains an array of gallery objects with `url` properties
           const galleries = response?.data?.galleries;
-        
+
           setImages(galleries);
-          console.log(galleries); 
+          console.log(galleries);
         }
       } catch (error) {
         console.log(error);
       }
     };
     fetchGalleryData();
-  
+
     setTimeout(() => {
-      setLoading(false); 
-    }, 2000); 
+      setLoading(false);
+    }, 2000);
   }, []);
-  
+
   const handleAddImage = async () => {
     if (!newImage && !fileInputRef.current?.files[0]) {
       console.error("No image selected!");
       return; // Ensure an image is selected before proceeding
     }
-  
+
     const formData = new FormData();
     const file = fileInputRef.current?.files[0]; // Get file from the input
-  
+
     formData.append("image", file);
-    formData.append("caption", imageName);
-  
+    formData.append("caption", caption);
+
     try {
       const response = await axiosPrivate.post(galleryRoute, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-  
+
       const addedImage = response.data.gallery;
-  
+
       // Add the full image object to the images array
       if (addedImage) {
         setImages((prevImages) => [...prevImages, addedImage]);
       }
-  
+
       toast.success("Image added successfully!");
       closeAddBox(); // Close the add image box after successful addition
-  
+
       // Reset form fields after successful image upload
       setImageName('');
       if (fileInputRef.current) {
@@ -218,35 +220,35 @@ const handleUpdateImage = async () => {
       console.error("Error in uploading image:", error);
     }
   };
-  
-//handleDeleteImage
 
-const handleRemoveImage = async (galleryId, index) => {
-  if (!galleryId) {
-    console.log("Gallery ID is undefined. Aborting delete operation.");
-    return;
-  }
-  
-  console.log("Deleting image with ID:", galleryId);
+  //handleDeleteImage
 
-  try {
-    await axiosPrivate.delete(`${galleryRoute}/${galleryId}`);
-    
-    const updatedImages = [...images];
-    const updatedIds = [...galleryIds];
-    updatedImages.splice(index, 1);
-    updatedIds.splice(index, 1);
-    
-    setImages(updatedImages);
-    setGalleryIds(updatedIds);
-    toast.success("Image deleted successfully!");
-    
-  } catch (error) {
-    console.log("Error in deleting image", error);
-    toast.error("Failed to delete image. Please try again.");
-  }
-};
-  
+  const handleRemoveImage = async (galleryId, index) => {
+    if (!galleryId) {
+      console.log("Gallery ID is undefined. Aborting delete operation.");
+      return;
+    }
+
+    console.log("Deleting image with ID:", galleryId);
+
+    try {
+      await axiosPrivate.delete(`${galleryRoute}/${galleryId}`);
+
+      const updatedImages = [...images];
+      const updatedIds = [...galleryIds];
+      updatedImages.splice(index, 1);
+      updatedIds.splice(index, 1);
+
+      setImages(updatedImages);
+      setGalleryIds(updatedIds);
+      toast.success("Image deleted successfully!");
+
+    } catch (error) {
+      console.log("Error in deleting image", error);
+      toast.error("Failed to delete image. Please try again.");
+    }
+  };
+
   const handleViewImage = (src) => {
     setCurrentImage(src);
     setShowImageModal(true);
@@ -273,13 +275,13 @@ const handleRemoveImage = async (galleryId, index) => {
     setCurrentImage(null);
   };
 
-  if (loading) return(
+  if (loading) return (
     <div className="h-screen w-full overflow-hidden">
 
-      <LoadingScreen/>
+      <LoadingScreen />
     </div>
-  ) 
-  
+  )
+
   return (
     <div className="min-h-screen flex flex-col w-full px-2 py-4"> {/* Reduced padding */}
       <h1 className="text-2xl font-bold text-primaryColor lg:hidden mt-[-10px] sm:mt-[-20px] text-left -ml-4">
@@ -322,6 +324,19 @@ const handleRemoveImage = async (galleryId, index) => {
               </div>
             </div>
 
+            <div className="mb-4">
+              <label className="block mb-2 text-sm text-gray-600 text-left">Caption</label>
+              {/* <div className="border border-gray-300 rounded-md p-2 flex items-center justify-between relative"> */}
+              <input
+                type="text"
+                value={caption}
+                onChange={(e) => setCaption(e.target.value)}
+                placeholder="Type here"
+                className="w-full border border-gray-300 rounded-md p-2"
+              />
+              {/* </div> */}
+            </div>
+
             {newImage && (
               <div className="mb-4 text-sm text-gray-600">
                 <div>
@@ -362,18 +377,17 @@ const handleRemoveImage = async (galleryId, index) => {
       )}
 
       {showImageModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-20 bg-black bg-opacity-75">
-          <div className="bg-white p-4 rounded-md relative w-1/2">
-            <button
+        <div className="fixed h-screen inset-0 flex items-center justify-center z-20 bg-black bg-opacity-75">
+          <div className="bg-white p-4 rounded-md relative w-1/2 max-h-screen ">
+            <RiCloseCircleFill
               onClick={closeImageModal}
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-            >
-              &times;
-            </button>
+              size={24}
+              className="z-40 absolute top-2 right-2 text-red-900"
+            />
             <img
               src={currentImage}
               alt="View"
-              className="w-full h-auto"
+              className="w-full "
             />
           </div>
         </div>
@@ -386,92 +400,91 @@ const handleRemoveImage = async (galleryId, index) => {
           msOverflowStyle: 'none', // IE and Edge
         }}
       >
-<div className="grid grid-cols-3 gap-x-5 gap-y-6 mt-4 px-2 py-2 -ml-9 -mr-6">
-  {images.map((item, index) => (
-    <div
-      key={index}
-      onClick={() => handleSelectImage(index)}
-      className={`relative border-2 ${
-        selectedImage === index ? "border-blue-400" : "border-transparent"
-      } cursor-pointer`}
-      style={{ marginTop: '-5px' }}
-    >
-      {item.image && item.image.location ? ( // Check if item.image and item.image.location exist
-        <img
-          src={item.image.location} // Correct image source
-          alt={`Gallery ${index}`}
-          className="w-full h-[130px] object-cover"
-          onClick={() => handleViewImage(item.image.location)}
-        />
-      ) : (
-        <div className="w-full h-[130px] bg-gray-200 flex items-center justify-center">
-          <span className="text-gray-500">No Image</span>
-        </div>
-      )}
+        <div className="grid grid-cols-3 gap-x-5 gap-y-6 mt-4 px-2 py-2 -ml-9 -mr-6">
+          {images.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => handleSelectImage(index)}
+              className={`relative border-2 ${selectedImage === index ? "border-blue-400" : "border-transparent"
+                } cursor-pointer`}
+              style={{ marginTop: '-5px' }}
+            >
+              {item.image && item.image.location ? ( // Check if item.image and item.image.location exist
+                <img
+                  src={item.image.location} // Correct image source
+                  alt={`Gallery ${index}`}
+                  className="w-full h-[130px] object-cover"
+                  onClick={() => handleViewImage(item.image.location)}
+                />
+              ) : (
+                <div className="w-full h-[130px] bg-gray-200 flex items-center justify-center">
+                  <span className="text-gray-500">No Image</span>
+                </div>
+              )}
 
-      {/* Optional edit and delete buttons */}
-      <div className="absolute top-2 right-2 space-y-2">
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            handleEditImage(index);
-          }}
-          className="text-gray-600 hover:text-primaryColor"
-        >
-          <EditIcon />
-        </button>
-
-        {isModalOpen && (
-          <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50">
-            <div className="bg-white p-6 rounded-md shadow-md relative w-80">
-              <h3 className="text-lg mb-4">Edit Image</h3>
-              <input
-                type="text"
-                value={editingImageName}
-                onChange={(e) => setEditingImageName(e.target.value)}
-                className="border p-2 mb-4 w-full"
-                placeholder="Edit caption"
-              />
-              <input
-                type="file"
-                onChange={handleEditFileChange}
-                className="mb-4"
-                accept="image/*"
-              />
-              <div className="flex justify-end space-x-2">
+              {/* Optional edit and delete buttons */}
+              <div className="absolute top-2 right-2 space-y-2">
                 <button
-                  onClick={handleUpdateImage}
-                  className="bg-primaryColor text-white px-4 py-2 rounded"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleEditImage(index);
+                  }}
+                  className="text-gray-600 hover:text-primaryColor"
                 >
-                  Save
+                  <EditIcon />
                 </button>
+
+                {isModalOpen && (
+                  <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50">
+                    <div className="bg-white p-6 rounded-md shadow-md relative w-80">
+                      <h3 className="text-lg mb-4">Edit Image</h3>
+                      <input
+                        type="text"
+                        value={editingImageName}
+                        onChange={(e) => setEditingImageName(e.target.value)}
+                        className="border p-2 mb-4 w-full"
+                        placeholder="Edit caption"
+                      />
+                      <input
+                        type="file"
+                        onChange={handleEditFileChange}
+                        className="mb-4"
+                        accept="image/*"
+                      />
+                      <div className="flex justify-end space-x-2">
+                        <button
+                          onClick={handleUpdateImage}
+                          className="bg-primaryColor text-white px-4 py-2 rounded"
+                        >
+                          Save
+                        </button>
+                        <button
+                          onClick={closeModal}
+                          className="text-gray-500 px-4 py-2 rounded hover:text-gray-700"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+
+
                 <button
-                  onClick={closeModal}
-                  className="text-gray-500 px-4 py-2 rounded hover:text-gray-700"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleRemoveImage(item._id, index);
+                  }}
+                  className="text-gray-600 hover:text-primaryColor"
                 >
-                  Cancel
+                  <TrashIcon />
                 </button>
               </div>
             </div>
-          </div>
-        )}
+          ))}
+        </div>
 
-      
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            handleRemoveImage(item._id, index);
-          }}
-          className="text-gray-600 hover:text-primaryColor"
-        >
-          <TrashIcon />
-        </button>
       </div>
-    </div>
-  ))}
-</div>
-
-</div>
     </div>
   );
 };
