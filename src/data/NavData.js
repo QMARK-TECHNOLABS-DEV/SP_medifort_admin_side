3d701738dcf00702375810985f9c680226da54da
Manu-Madhu <manum66466@gmail.com>
2025-04-08 13:23:54 +0530

    Icon added

--- src/data/NavData.js
+++ src/data/NavData.js
@@ -1,94 +1,72 @@
+import {
+  Home,
+  User,
+  LayoutGrid,
+  FileText,
+  TicketSlash,
+  TvMinimalPlay,
+  UserSearch
+} from "lucide-react";
 export const NavData = [
-    {
-        id: 1,
-        name: "Home",
-        link: "/",
-        list: []
-    },
-    {
-        id: 2,
-        name: "Doctor profile",
-        link: "/doctors",
-        list: []
-    },
-    {
-        id: 3,
-        name: "Department",
-        link: "/department",
-        list: []
-    },
-    {
-        id: 4,
-        name: "Content Management",
-        link: "/content-management",
-        list: []
-    },
-    {
-        id: 5,
-        name: "Banner Management",
-        link: "/banner-management",
-        list: []
-    },
-    {
-        id: 6,
-        name: "Testimonials",
-        link: "/testimonials",
-        list: []
-    },
-    {
-        id: 7,
-        name: "Enquiries",
-        link: "/enquiry",
-        list: []
-    },
-    // {
-    //     "id": 3,
-    //     "title": "Media",
-    //     "link": "/user/media",
-    //     "list": [
-    //         {
-    //             "lid": 1,
-    //             "subTitle": "news",
-    //             "subLink": "/user/news"
-    //         },
-    //         {
-    //             "lid": 2,
-    //             "subTitle": "events",
-    //             "subLink": "/user/events"
-    //         },
-    //         {
-    //             "lid": 3,
-    //             "subTitle": "publications",
-    //             "subLink": "/user/publication"
-    //         },
-    //         {
-    //             "lid": 4,
-    //             "subTitle": "newsletters",
-    //             "subLink": "/user/newsletter"
-    //         },
-    //     ]
-    // },
-    // {
-    //     "id": 4,
-    //     "title": "WSO Initiatives",
-    //     "link": "/user/wso-initiatives",
-    //     "list": [
-    //         {
-    //             "lid": 1,
-    //             "subTitle": "NSC",
-    //             "subLink": "/user/nsc"
-    //         },
-    //         {
-    //             "lid": 2,
-    //             "subTitle": "NSS",
-    //             "subLink": "/user/nss"
-    //         }
-    //     ]
-    // },
-    // {
-    //     "id": 5,
-    //     "title": "Contact Us",
-    //     "link": "/contact-us",
-    //     "list": []
-    // }
-]
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+    icon: <Home />,
+    list: []
+  },
+  {
+    id: 2,
+    name: "Doctor profile",
+    link: "/doctors",
+    icon: <User />,
+    list: []
+  },
+  {
+    id: 3,
+    name: "Department",
+    link: "/department",
+    icon: <LayoutGrid />,
+    list: []
+  },
+  {
+    id: 4,
+    name: "Content Management",
+    link: "/content-management",
+    icon: <TvMinimalPlay />,
+    list: []
+  },
+  {
+    id: 5,
+    name: "Banner Management",
+    link: "/banner-management",
+    icon: <TicketSlash />,
+    list: []
+  },
+  {
+    id: 6,
+    name: "Testimonials",
+    link: "/testimonials",
+    icon: <FileText />,
+    list: []
+  },
+  {
+    id: 7,
+    name: "Enquiries",
+    link: "/enquiry",
+    icon: <UserSearch />,
+    list: []
+  }
+  // {
+  //     "id": 4,
+  //     "title": "WSO Initiatives",
+  //     "link": "/user/wso-initiatives",
+  //     "list": [
+  //         {
+  //             "lid": 1,
+  //             "subTitle": "NSC",
+  //             "subLink": "/user/nsc"
+  //         },
+  //     ]
+  // },
+];
