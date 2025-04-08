f67e7cf58630df189098bff3671d3b40f7d2c406
AnandhuRs619 <ananthurs619@gmail.com>
2025-04-08 18:09:33 +0530

    logout button is added on the Sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { NavData } from "../../data/NavData";
+import { FiLogOut } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../redux/slices/AuthSlicer";
+import { setAccessToken, setRefreshToken } from "../../redux/slices/TokenReducer";
 
 const Sidebar = () => {
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(setUser(null));
+    dispatch(setAccessToken(null));
+    dispatch(setRefreshToken(null));
+  };
 
   return (
-    <div className="mt-3 mb-20 h-full">
-      <ul className="flex flex-col gap-1 h-full ">
+    <div className="h-full flex flex-col justify-between overflow-y-auto max-h-[calc(100vh-8rem)]">
+      {/* Top navigation items */}
+      <ul className="flex flex-col gap-1">
         {NavData?.map((item) => {
           const isActive =
             pathname === item?.link ||
@@ -27,6 +39,16 @@ const Sidebar = () => {
           );
         })}
       </ul>
+      {/* Logout button at bottom */}
+      <div className="border-t-2 border-primaryColor pt-2">
+        <div
+          onClick={handleLogout}
+          className="p-2 flex items-center gap-2 hover:bg-primaryColor text-black hover:text-white rounded cursor-pointer font-[600] text-[18px] text-start h-[50px]"
+        >
+          <FiLogOut size={26} />
+          Logout
+        </div>
+      </div>
     </div>
   );
 };
